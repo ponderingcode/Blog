473988e5b1f528ce74bf13013cbf1899ddcfe01f
Ryan Fleming <ryan@ponderingcode.com>
2016-07-21 16:26:00 -0400

    added new user (moderator role)

--- Blog/Migrations/Configuration.cs
+++ Blog/Migrations/Configuration.cs
@@ -3,13 +3,14 @@
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Blog.Models.ApplicationDbContext>
     {
+        private const string ADMINISTRATOR = "Administrator";
+        private const string MODERATOR = "Moderator";
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
@@ -18,14 +19,19 @@
         {
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-            if (!context.Roles.Any(r => r.Name == "Admin"))
+            if (!context.Roles.Any(r => r.Name == ADMINISTRATOR))
             {
-                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = ADMINISTRATOR });
+            }
+
+            if (!context.Roles.Any(r => r.Name == MODERATOR))
+            {
+                roleManager.Create(new IdentityRole { Name = MODERATOR });
             }
 
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-            if (!context.Users.Any(u => u.Email == "your email address"))
+            if (!context.Users.Any(u => u.Email == "ryan@ponderingcode.com"))
             {
                 userManager.Create(new ApplicationUser
                 {
@@ -37,8 +43,23 @@
                 }, "CoderFoundry");
             }
 
-            var userId = userManager.FindByEmail("ryan@ponderingcode.com").Id;
-            userManager.AddToRole(userId, "Admin");
+            var administratorUserID = userManager.FindByEmail("ryan@ponderingcode.com").Id;
+            userManager.AddToRole(administratorUserID, ADMINISTRATOR);
+
+            if (!context.Users.Any(u => u.Email == "jtwichell@coderfoundry.com"))
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "jtwichell@coderfoundry.com",
+                    Email = "jtwichell@coderfoundry.com",
+                    FirstName = "Jason",
+                    LastName = "Twichell",
+                    DisplayName = "J-Twich",
+                }, "Abc&123!");
+            }
+
+            var moderatorUserId = userManager.FindByEmail("jtwichell@coderfoundry.com").Id;
+            userManager.AddToRole(moderatorUserId, MODERATOR);
         }
     }
 }
